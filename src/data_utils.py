59f4372ea356d3b16d33e3021ee7e0586db8605c
Tomáš Mlynář <tom.mlynar1@gmail.com>
2024-04-28 11:28:06 +0200

    refactoring, new directories

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+from typing import List
 
-def load_vocabulary(path="data/bag+of+words/vocab.kos.txt"):
+def load_vocabulary(path:str="data/bag+of+words/vocab.kos.txt") -> List[str]:
     vocabulary = []
     with open(path, "r") as f:
         for line in f:
@@ -9,12 +10,12 @@ def load_vocabulary(path="data/bag+of+words/vocab.kos.txt"):
     return vocabulary
 
 
-def load_docwords(path="data/bag+of+words/docword.kos.txt"):
+def load_docwords(path:str="data/bag+of+words/docword.kos.txt") -> np.ndarray:
     with open(path, "r") as f:
         n_docs = int(f.readline().strip())
         n_words = int(f.readline().strip())
         n_nonzero_counts = int(f.readline().strip())
-        print(n_docs, n_words, n_nonzero_counts)
+        
         counts_matrix = np.zeros((n_docs, n_words))
         for line in f:
             split_line = line.strip().split(" ")
---
+++ src/doc_landscape.py
@@ -0,0 +1,65 @@
+import math
+import random
+import sys
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import (QAction, QBrush, QColor, QKeySequence, QPainter,
+                           QPen, QSurfaceFormat, QTransform)
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+from PySide6.QtWidgets import (QApplication, QGraphicsScene, QGraphicsView,
+                               QHBoxLayout, QHeaderView, QMainWindow, QMenuBar,
+                               QSizePolicy, QWidget)
+
+
+class VisGraphicsScene(QGraphicsScene):
+    def __init__(self):
+        super(VisGraphicsScene, self).__init__()
+        self.selection = None
+        self.wasDragg = False
+        self.pen = QPen(Qt.black)
+        self.selected = QPen(Qt.red)
+
+    def mouseReleaseEvent(self, event): 
+        if(self.wasDragg):
+            return
+        if(self.selection):
+            self.selection.setPen(self.pen)
+        item = self.itemAt(event.scenePos(), QTransform())
+        if(item):
+            item.setPen(self.selected)
+            self.selection = item
+
+
+class VisGraphicsView(QGraphicsView):
+    def __init__(self, scene, parent):
+        super(VisGraphicsView, self).__init__(scene, parent)
+        self.startX = 0.0
+        self.startY = 0.0
+        self.distance = 0.0
+        self.myScene = scene
+        self.setRenderHints(QPainter.Antialiasing | QPainter.TextAntialiasing | QPainter.SmoothPixmapTransform)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.AnchorViewCenter)
+
+    def wheelEvent(self, event):
+        zoom = 1 + event.angleDelta().y()*0.001;
+        self.scale(zoom, zoom)
+        
+    def mousePressEvent(self, event):
+        self.startX = event.position().x()
+        self.startY = event.position().y()
+        self.myScene.wasDragg = False
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        endX = event.position().x()
+        endY = event.position().y()
+        deltaX = endX - self.startX
+        deltaY = endY - self.startY
+        distance = math.sqrt(deltaX*deltaX + deltaY*deltaY)
+        if(distance > 5):
+            self.myScene.wasDragg = True
+        super().mouseReleaseEvent(event)
