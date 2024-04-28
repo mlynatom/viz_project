2ebe9bf3e85781439427591e404f9c7792454c2c
Tomáš Mlynář <tom.mlynar1@gmail.com>
2024-04-28 13:08:59 +0200

    basic data view

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import random
 import sys
+import numpy as np
 
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtGui import (QAction, QBrush, QColor, QKeySequence, QPainter,
@@ -11,6 +12,7 @@ from PySide6.QtWidgets import (QApplication, QGraphicsScene, QGraphicsView,
                                QSizePolicy, QWidget)
 
 from src.doc_landscape import VisGraphicsScene, VisGraphicsView
+from src.data_utils import DocumentData
 
 
 class MainWindow(QMainWindow):
@@ -20,6 +22,12 @@ class MainWindow(QMainWindow):
     def __init__(self, central_widget: QWidget):
         super(MainWindow, self).__init__()
         self.setWindowTitle('Document Corpus Visualization')
+
+        #window dimensions
+        geometry = self.screen().availableGeometry()
+        #self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
+        self.setMinimumSize(geometry.width() * 0.5, geometry.height() * 0.4)
+      
         #set central widget
         self.setCentralWidget(central_widget)
 
@@ -37,11 +45,7 @@ class MainWindow(QMainWindow):
         self.status_bar = self.statusBar()
         self.status_bar.showMessage("Data loaded and plotted")
 
-        #window dimensions
-        geometry = self.screen().availableGeometry()
-        #self.setFixedSize(geometry.width() * 0.8, geometry.height() * 0.7)
-        self.setMinimumSize(geometry.width() * 0.5, geometry.height() * 0.4)
-      
+        
         self.show()
 
 class CentralWidget(QWidget):
@@ -53,7 +57,7 @@ class CentralWidget(QWidget):
 
         #init subwidget
         self.scene = VisGraphicsScene()
-        self.brush = [QBrush(Qt.yellow), QBrush(Qt.green), QBrush(Qt.blue)]
+        self.brush = [QBrush(Qt.yellow), QBrush(Qt.green), QBrush(Qt.blue), QBrush(Qt.red), QBrush(Qt.cyan), QBrush(Qt.magenta), QBrush(Qt.gray), QBrush(Qt.darkYellow), QBrush(Qt.darkGreen), QBrush(Qt.darkBlue), QBrush(Qt.darkRed), QBrush(Qt.darkCyan)]
         
         format = QSurfaceFormat()
         format.setSamples(4)
@@ -66,10 +70,15 @@ class CentralWidget(QWidget):
         self.view.setViewport(gl)
         self.view.setBackgroundBrush(QColor(255, 255, 255))
 
+        #TODO load all data
+        self.document = DocumentData(data_path="data/bag+of+words", name="kos")
+        self.document_coords = self.document.fit_transform("pca")
+        self.doc_topic, self.topic = self.document.nmf(n_components=12, num_topic_words=5)
+
         #add data
         self.generateAndMapData()
 
-        #init subwidget
+        #init subwidget TODO change to table
         self.scene2 = VisGraphicsScene()
         
         format2 = QSurfaceFormat()
@@ -84,9 +93,7 @@ class CentralWidget(QWidget):
         self.view2.setBackgroundBrush(QColor(255, 255, 255))
 
         #add data
-        self.generateAndMapData()
-        
-
+        #self.generateAndMapData()
         
         #set layout for table right and visualization left
         self.main_layout = QHBoxLayout()
@@ -95,7 +102,7 @@ class CentralWidget(QWidget):
 
         #left layout
         size.setHorizontalStretch(4)
-        #self.view.setGeometry(0, 0, 800, 600)
+        self.view.setGeometry(0, 0, 800, 600)
         self.view.setSizePolicy(size)
         self.main_layout.addWidget(self.view)
 
@@ -109,21 +116,50 @@ class CentralWidget(QWidget):
 
     def generateAndMapData(self):
         #Generate random data
-        count = 100
-        x = []
-        y = []
-        r = []
-        c = []
-        for i in range(0, count):
-            x.append(random.random()*600)
-            y.append(random.random()*400)
-            r.append(random.random()*50)
-            c.append(random.randint(0, 2))
+        # count = 100
+        # x = []
+        # y = []
+        # r = []
+        # c = []
+        # for i in range(0, count):
+        #     x.append(random.random()*600)
+        #     y.append(random.random()*400)
+        #     r.append(random.random()*50)
+        #     c.append(random.randint(0, 2))
+
+        # #Map data to graphical elements
+        # for i in range(0, count):
+        #     d = 2*r[i]
+        #     ellipse = self.scene.addEllipse(x[i], y[i], d, d, self.scene.pen, self.brush[c[i]])
+        
+        width = self.scene.width()
+        height = self.scene.height()
+        print(width)
+        print(height)
+
+        #Generate random data
+        #remap the result of pca to the screen
+        x = self.document_coords[:, 0]
+        y = self.document_coords[:, 1]
+        x_min = np.min(x)
+        x_max = np.max(x)
+        y_min = np.min(y)
+        y_max = np.max(y)
+
+        x_min_max_scaled = (x - x_min) / (x_max - x_min)
+        y_min_max_scaled = (y - y_min) / (y_max - y_min)
+
+        #rescale minmaxed to the screen
+        width = 800
+        height = 600
+        x = x_min_max_scaled * width
+        y = y_min_max_scaled * height
+        c = self.doc_topic #get colors
 
         #Map data to graphical elements
-        for i in range(0, count):
-            d = 2*r[i]
-            ellipse = self.scene.addEllipse(x[i], y[i], d, d, self.scene.pen, self.brush[c[i]])
+        for i in range(0, x.shape[0]):
+            d = 3
+            ellipse = self.scene.addEllipse(x[i], y[i], d,d, self.scene.pen, self.brush[c[i]])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- src/data_utils.py
+++ src/data_utils.py
@@ -1,25 +1,70 @@
+import os
+from typing import List, Literal, Union
+
 import numpy as np
-from typing import List
+from sklearn.decomposition import NMF  # TODO write own
+from sklearn.decomposition import PCA  # TODO write own
+from sklearn.feature_extraction.text import TfidfTransformer
 
-def load_vocabulary(path:str="data/bag+of+words/vocab.kos.txt") -> List[str]:
-    vocabulary = []
-    with open(path, "r") as f:
-        for line in f:
-            vocabulary.append(line.strip("\n"))
+SEED = 42
 
-    return vocabulary
-
-
-def load_docwords(path:str="data/bag+of+words/docword.kos.txt") -> np.ndarray:
-    with open(path, "r") as f:
-        n_docs = int(f.readline().strip())
-        n_words = int(f.readline().strip())
-        n_nonzero_counts = int(f.readline().strip())
+class DocumentData():
+    def __init__(self, data_path:str, name:str) -> None:
+        self.vocab_path = os.path.join(data_path, f"vocab.{name}.txt")
+        self.docword_path = os.path.join(data_path, f"docword.{name}.txt")
+        self.vocabulary = self._load_vocabulary()
+        self.doc_words_matrix = self._load_docwords()
+        self.tfidf_matrix = TfidfTransformer().fit_transform(self.doc_words_matrix)
         
-        counts_matrix = np.zeros((n_docs, n_words))
-        for line in f:
-            split_line = line.strip().split(" ")
-            counts_matrix[int(split_line[0])-1,
-                          int(split_line[1])-1] = int(split_line[2])
+    def fit_transform(self, solver: Union[Literal["pca"], Literal["umap"], Literal["tsne"]]):
+        if solver == "pca":
+            return self._pca()
+
+
+    def _load_vocabulary(self) -> List[str]:
+        vocabulary = []
+        with open(self.vocab_path, "r") as f:
+            for line in f:
+                vocabulary.append(line.strip("\n"))
+
+        return vocabulary
     
-    return counts_matrix
+
+    def _load_docwords(self) -> np.ndarray:
+        with open(self.docword_path, "r") as f:
+            self.n_docs = int(f.readline().strip())
+            self.n_words = int(f.readline().strip())
+            self.n_nonzero_counts = int(f.readline().strip())
+
+            counts_matrix = np.zeros((self.n_docs, self.n_words))
+            for line in f:
+                split_line = line.strip().split(" ")
+                counts_matrix[int(split_line[0])-1,
+                              int(split_line[1])-1] = int(split_line[2])
+
+        return counts_matrix
+    
+    def _pca(self):
+        pca = PCA(n_components=2, svd_solver="arpack")
+        return pca.fit_transform(self.tfidf_matrix)
+    
+    def nmf(self, n_components:int = 10, num_topic_words: int = 5):
+        nmf = NMF(n_components=n_components, random_state=SEED)
+        W_matrix = nmf.fit_transform(self.tfidf_matrix) # document X topics matrix
+        H_matrix = nmf.components_ # topics X words matrix
+
+        topics_nmf = np.argmax(W_matrix, axis=1)
+        topics_words = self._get_topics_words(H_matrix, num_topic_words)
+
+        return topics_nmf, topics_words
+
+    def _get_topics_words(self, topic_words_matrix, n:int = 5) -> List[List[str]]:
+        topics_words = []
+
+        for i in range(topic_words_matrix.shape[0]):
+            ind =  np.argpartition(topic_words_matrix[i, :], -n)[-n:]
+
+            words = [self.vocabulary[word_id] for word_id in ind]
+            topics_words.append(words)
+
+        return topics_words 
